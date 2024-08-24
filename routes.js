8ad4b4b7b5d6edd7e2fe5dfdbbf05dd78146e3be
Tony Kan <tianchengkan@gmail.com>
2024-08-24 12:43:51 -0700

    fix: Correct route order and ensure room creation works as expected
    - Moved 'create' route to be processed before 'join' route to avoid routing conflicts.
    - Fixed routing bug to ensure new rooms are created and redirected correctly.
    
    This commit resolves a routing issue by ensuring that room creation is handled before room joining.

--- routes.js
+++ routes.js
@@ -7,15 +7,15 @@
 const express = require("express");
 const router = express.Router();
 
-// Join the room
-router.get("/:room", (req, res) => {
-  res.sendFile("index.html", { root: "public" });
-});
-
 // Create a room
 router.get("/create", (req, res) => {
   const roomId = String(Math.floor(Math.random() * 100000));
   res.redirect(`/${roomId}`);
+});
+
+// Join the room
+router.get("/:room", (req, res) => {
+  res.sendFile("index.html", { root: "public" });
 });
 
 module.exports = router;
