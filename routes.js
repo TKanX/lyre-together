111ff46e54a57a1b95b5ff6d60c1e4b267c39720
Tony Kan <tianchengkan@gmail.com>
2024-08-24 12:37:24 -0700

    feat: Add routes for joining and creating rooms
    - Implemented route to handle room joining by serving 'index.html' for the specified room.
    - Implemented route to create a new room with a random ID and redirect to the new room URL.
    
    This commit adds server-side routing for room management, enabling users to join existing rooms or create new ones with unique IDs.

--- routes.js
+++ routes.js
@@ -7,4 +7,15 @@
 const express = require("express");
 const router = express.Router();
 
+// Join the room
+router.get("/:room", (req, res) => {
+  res.sendFile("index.html", { root: "public" });
+});
+
+// Create a room
+router.get("/create", (req, res) => {
+  const roomId = String(Math.floor(Math.random() * 100000));
+  res.redirect(`/${roomId}`);
+});
+
 module.exports = router;
